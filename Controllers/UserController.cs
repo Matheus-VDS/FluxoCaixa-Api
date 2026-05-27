803a53f837d35c881e26aa20d6914b04c0b558a9
Matheus Vieira de Souza <101958548+Matheus-VDS@users.noreply.github.com>
2026-05-26 21:16:18 -0400

    Commit inicial com o back-end do projeto

---
+++ Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FluxoCaixa_Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Usuario 1", "Usuario 2", "Chilly", "Cool", "Mild", "Warm"
+        };
+
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetUser")]
+        public IEnumerable<User> Get()
+        {
+            return new User[]
+            {
+                new User { Name = "Teste", Role = "Comum" }
+            };
+        }
+    }
+}
+
---
+++ User.cs
@@ -0,0 +1,15 @@
+﻿namespace FluxoCaixa_Api
+{   // entidade user
+    public class User
+    {
+        public int Id { get; set; }
+        public int CPF_CNPJ { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public int Phone { get; set; }
+        public string Address { get; set; }
+        public int PermissionLevel { get; set; }
+    }
+}
